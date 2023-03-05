9aa4f302ba354ca2d1314d888dfe32cb4fb3e7a0
Paweł Worwąg <pawel.worwag@gmail.com>
2023-03-05 11:46:56 +0100

    Database context

---
+++ src/Mailer.Application/Common/Interfaces/IMailerDbContext.cs
@@ -0,0 +1,13 @@
+using Mailer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mailer.Application.Common.Interfaces;
+
+public interface IMailerDbContext
+{
+    DbSet<Attachment> Attachments { get; set; }
+    DbSet<EmailType> EmailTypes { get; set; }
+    DbSet<Language> Languages { get; set; }
+    DbSet<Template> Templates { get; set; }
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
