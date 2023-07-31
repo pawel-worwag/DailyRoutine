55639dfaed67d9a23fde1119e16e318cbb56c184
Paweł Worwąg <pawel.worwag@gmail.com>
2023-07-31 22:45:25 +0200

    User claims.

--- src/Identity/Identity.Domain/User.cs
+++ src/Identity/Identity.Domain/User.cs
@@ -14,22 +14,26 @@ public class User
     public string PasswordHash { get; set; }
     public HashSet<Role> Roles { get; private set; } = new HashSet<Role>();
     protected ICollection<UserClaim> UserClaims { get; private set; } = new List<UserClaim>();
+
+    /// <summary>
+    /// Claims added to user.
+    /// </summary>
+    [NotMapped]
+    public IReadOnlyCollection<Claim> PrivateClaims
+    {
+        get { return UserClaims.Select(p => new Claim(p.ClaimType, p.ClaimValue)).ToList(); }
+    }
+
+    /// <summary>
+    /// Union of claims added to user and claims added to user's roles.
+    /// </summary>
     [NotMapped]
     public IReadOnlyCollection<Claim> Claims
     {
         get
         {
-            var claims = new List<Claim>();
-            foreach (var c in UserClaims)
-            {
-                claims.Add(new Claim(c.ClaimType,c.ClaimValue));
-            }
-
-            foreach (var c in Roles.SelectMany(p=>p.Claims))
-            {
-                claims.Add(new Claim(c.ClaimType,c.ClaimValue));
-            }
-            return claims;
+            return UserClaims.Select(p => new Claim(p.ClaimType, p.ClaimValue))
+                .Union(Roles.SelectMany(p => p.Claims.Select(c => new Claim(c.ClaimType, c.ClaimValue)))).ToList();
         }
     }
 }
