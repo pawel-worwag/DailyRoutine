dfb7264dbb66199a0153f75070b1ec2fd495e8ca
Paweł Worwąg <pawel.worwag@gmail.com>
2023-08-19 20:01:44 +0200

    NotFound exceptions - override HttpCode and Error properties.

--- src/Mailer/Mailer.Infrastructure/Common/Exceptions/NotFoundException.cs
+++ src/Mailer/Mailer.Infrastructure/Common/Exceptions/NotFoundException.cs
@@ -1,7 +1,11 @@
+using System.Net;
+
 namespace Mailer.Infrastructure.Common.Exceptions;
 
 public class NotFoundException : InfrastructureException
 {
+    public override HttpStatusCode HttpCode { get; protected set; } = HttpStatusCode.NotFound;
+    public override string Error { get; protected set; } = "not_found";
     public NotFoundException(string? message) : base(message)
     {
     }
