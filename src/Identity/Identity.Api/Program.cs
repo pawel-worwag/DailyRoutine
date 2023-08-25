a341622468f81f7ff3e347b2054b5805faa28f66
Paweł Worwąg <pawel.worwag@gmail.com>
2023-08-25 21:07:24 +0200

    Swagger custom schema ids

--- src/Identity/Identity.Api/Program.cs
+++ src/Identity/Identity.Api/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen( options => {
     var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+    options.CustomSchemaIds(type => type.FullName?.Replace("Identity.Application.",""));
 });
 
 
