62f8a1e27932f694d4320150e6d3ae50d7df882d
Paweł Worwąg <pawel.worwag@gmail.com>
2023-08-27 10:31:51 +0200

    Endpoint concept - manage clients

--- src/Identity/Identity.Api/Controllers/ClientController.cs
+++ src/Identity/Identity.Api/Controllers/ClientController.cs
@@ -32,4 +32,37 @@ public class ClientController : ControllerBase
     {
         return Ok(await _mediator.Send(new Application.Clients.GetClients.GetClientsRequest()));
     }
+
+    /// <summary>
+    /// [TO-DO] Add new api client
+    /// </summary>
+    /// <returns></returns>
+    [HttpPost]
+    public ActionResult AddClient()
+    {
+        return Ok();
+    }
+    
+    /// <summary>
+    /// [TO-DO] Update api client
+    /// </summary>
+    /// <param name="guid"></param>
+    /// <returns></returns>
+    [HttpPost("{guid:guid}")]
+    public ActionResult UpdateClient(Guid guid)
+    {
+        return Ok();
+    }    
+    
+    /// <summary>
+    /// [TO-DO] Remove api client
+    /// </summary>
+    /// <param name="guid"></param>
+    /// <returns></returns>
+    [HttpDelete("{guid:guid}")]
+    public ActionResult RemoveClient(Guid guid)
+    {
+        return Ok();
+    }
+    
 }
