3c5df45bdb1718fc2ca17bfe37637900bcce1c02
Paweł Worwąg <pawel.worwag@gmail.com>
2023-10-06 20:28:54 +0200

    Exceptions handling

--- src/Identity/Identity.Application/Auth/GetJwks/GetJwks.cs
+++ src/Identity/Identity.Application/Auth/GetJwks/GetJwks.cs
@@ -1,6 +1,7 @@
 using System.Security.Cryptography.X509Certificates;
 using Identity.Application.Common.Options;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Identity.Application.Auth.GetJwks;
@@ -10,9 +11,11 @@ public record GetJwksRequest() : IRequest<Jwks>;
 internal class GetJwksHandler : IRequestHandler<GetJwksRequest, Jwks>
 {
     private readonly JwksOptions _jwks;
+    private readonly ILogger _logger;
 
-    public GetJwksHandler(IOptions<JwksOptions> jwks)
+    public GetJwksHandler(IOptions<JwksOptions> jwks, ILogger<GetJwksHandler> logger)
     {
+        _logger = logger;
         _jwks = jwks.Value;
     }
 
@@ -21,31 +24,46 @@ internal class GetJwksHandler : IRequestHandler<GetJwksRequest, Jwks>
         var keys = new List<Key>();
         foreach (var file in _jwks.Keys)
         {
-            var c = file.KeyPassword is null
-                ? new X509Certificate2(file.KeyFile)
-                : new X509Certificate2(file.KeyFile, file.KeyPassword);
-            if (c.HasPrivateKey)
+            if (!File.Exists(file.KeyFile))
             {
+                _logger.LogWarning("File '{file}' not exists.", file.KeyFile);
+                continue;
+            }
+
+            try
+            {
+                var c = file.KeyPassword is null
+                    ? new X509Certificate2(file.KeyFile)
+                    : new X509Certificate2(file.KeyFile, file.KeyPassword);
+
+                var n = DateTime.UtcNow;
+                if ((c.NotAfter < n) || (c.NotBefore > n)) continue;
+
+                if (!c.HasPrivateKey) continue;
+
                 var key = c.GetRSAPrivateKey();
-                if (key is not null)
+                if (key is null) continue;
+
+                var p = key.ExportParameters(false);
+                keys.Add(new Key()
                 {
-                    var p = key.ExportParameters(false);
-                    keys.Add(new Key()
-                    {
-                        Kty = key.SignatureAlgorithm,
-                        E = (p.Exponent is null)?"":Convert.ToBase64String(p.Exponent)!,
-                        Kid = file.Kid,
-                        Alg = c.SignatureAlgorithm.FriendlyName!,
-                        N = (p.Modulus is null )?"":Convert.ToBase64String(p.Modulus)!,
-                        X5t = c.Thumbprint,
-                        X5c = new List<string>(){c.Thumbprint},
-                        Use = "sig"
-                    });
-                }
+                    Kty = key.SignatureAlgorithm,
+                    E = (p.Exponent is null) ? "" : Convert.ToBase64String(p.Exponent)!,
+                    Kid = file.Kid,
+                    Alg = c.SignatureAlgorithm.FriendlyName!,
+                    N = (p.Modulus is null) ? "" : Convert.ToBase64String(p.Modulus)!,
+                    X5t = c.Thumbprint,
+                    X5c = new List<string>() { c.Thumbprint },
+                    Use = "sig"
+                });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarning("Error when opening file '{file}'. {message}", file.KeyFile,ex.Message);
             }
         }
 
         await Task.CompletedTask;
-        return new Jwks{Keys = keys};
+        return new Jwks { Keys = keys };
     }
 }
